1378fd9c1b148911a174bb4fb02f6efb23557c29
Sam <samuel1008@gmail.com>
2016-04-15 23:50:37 -0700

    finally got update to update

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -9,22 +9,24 @@ function BooksShowController($http, $routeParams, $location) {
     method: 'GET',
     url: 'https:/super-crud.herokuapp.com/books/'+$routeParams.id
   }).then(function successCallback(json) {
-
     vm.book = json.data;
   });
 
     vm.editBook = function (book) {
+      console.log(book);
     $http({
       method: 'PUT',
       url: 'https:/super-crud.herokuapp.com/books/'+$routeParams.id,
       data: {
-        title : bookToUpdate.title,
-        author : bookToUpdate.author,
-        image : bookToUpdate.image,
-        releaseDate : bookToUpdate.releaseDate
+        title: vm.book.title,
+        author: vm.book.author,
+        releaseDate:vm.book.releaseDate,
+        image: vm.book.image
       }
-      }).then(function successCallback(json) {
-      // don't need to do anything!
+    }).then(function successCallback(response) {
+         console.log('here\'s the UPDATED data for book', $routeParams.id, ':', response.data);
+        vm.book = response.data;
+        $location.path('/');
       }, function errorCallback(response) {
         console.log('There was an error editing the data', response);
       });
